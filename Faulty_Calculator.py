9fe56a74afa9f47ca449c3c82f2eda4805de3f02
Anna <CodeWithAnna@gmail.com>
2021-02-08 13:55:01 +0530

    Deleted Comments

--- Faulty_Calculator.py
+++ Faulty_Calculator.py
@@ -1,24 +1,3 @@
-# #excercise
-# #faulty calculator
-# # design a caluc-e operator and the two numbers as input from the user and then return the result
-#
-# Scenario:
-# Suppose that you are an invigilator in an exam. Calculator is not allowed for the exam. You discover somehow that
-# students are planning to cheat in exam, using a calculator program in Python language. Somehow you get your hands
-# on the calculator program and now you want to alter few results in calculator with your own provided results,
-# so you can catch the students who are trying to cheat using the calculator program.
-#
-# The user will provide the following inputs:
-#
-# Operator
-# The two numbers, between which the operator is applied
-# All the results will be correct, except for these few:
-#
-# 45 * 3 = 555
-# 56+9 = 77
-# 56/6 = 4
-
-
 print("Enter 1st number")
 num1 = int(input())
 print("Enter 2nd number")
